3d65c64f403d3e48b76d9680417d384552eeba2a
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-06-27 22:04:17 +0200

    withplayer

--- js/obstacle.js
+++ js/obstacle.js
@@ -4,7 +4,7 @@ class Obstacle {
       this.width = 100;
       this.height = 100;
       this.element = document.createElement('img');
-      this.element.src = './Images/whitewalker.png';
+      this.element.src = './images/whitewalker.png';
       this.element.style.position = 'absolute';
       this.element.style.width = `${this.width}px`;
       this.element.style.height = `${this.height}px`;
